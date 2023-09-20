00fd1f70fea40992c1fdd9d696c0c61e499d7c28
Gourav Yadav <gouravy1203@gmail.com>
2023-09-20 23:20:48 +0530

    Data Transformation Is Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,7 +4,8 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
-
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
@@ -50,7 +51,10 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
         
