d6cc7b86e92ce219535a668237727cff636d5513
Gourav Yadav <gouravy1203@gmail.com>
2023-09-20 23:57:13 +0530

    origin

---
+++ src/components/model_trainer.py
@@ -0,0 +1,53 @@
+import os 
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts","Model.pkl")
+
+class ModelTrainer:
+    def __init(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+    def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
+        try:
+            logging.info("Split training and test input data")
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            models = (
+                LinearRegression(),
+                DecisionTreeRegressor(),
+                RandomForestRegressor(),
+                AdaBoostRegressor(),
+                GradientBoostingRegressor(),
+                XGBRegressor(),
+                KNeighborsRegressor(),
+                CatBoostRegressor(),
+            )
+            
+            logging.info('Load preprocessor')
+        except:
+            pass
+
