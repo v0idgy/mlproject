46e706e338daa9c48860eaf2cf807866eb038c4d
Gourav Yadav <gouravy1203@gmail.com>
2023-09-21 21:27:54 +0530

    origin

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -17,7 +17,7 @@ from xgboost import XGBRegressor
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object
+from src.utils import save_object,evaluate_model 
 
 @dataclass
 class ModelTrainerConfig:
@@ -47,7 +47,23 @@ class ModelTrainer:
                 CatBoostRegressor(),
             )
 
-            # model_report: dict = evaluate_model{X_train=X_train,Y_train=y_train,X_test,y_test,models=models}
+            model_report: dict = evaluate_model(X_train=X_train,y_train=Y_train,x_test=X_test,y_test=y_test,models=models)
+
+            ## To get the best model score from the dict
+
+            best_model_score = max(sorted(model_report.values()))
+
+            ## To get the best model name from the dict
+
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+
+            best_model = models[best_model_name]
+
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            
+            logging.info("Best found model on both trainig and testing dataset")
+
             
            
         except:
