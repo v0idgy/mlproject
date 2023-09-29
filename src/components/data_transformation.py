f67de00da951e6191f20c39a8cb7e49c1b39d4a5
Gourav Yadav <gouravy1203@gmail.com>
2023-09-29 16:42:30 +0530

    update

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -112,7 +112,7 @@ class DataTransformation:
                 train_arr,
                 test_arr,
                 self.data_transformation_config.preprocessor_obj_file_path,
-            )
+            ) 
         except Exception as e:
             raise CustomException(e,sys)
  
